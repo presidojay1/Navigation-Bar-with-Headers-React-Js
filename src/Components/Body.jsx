6c845450a6b2be07117fad594655360be8e64cd2
Presido <omojoladavid08@gmail.com>
2022-09-06 09:43:37 -0700

    add

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -35,97 +35,101 @@ const Body = () => {
             <h2>107</h2>
           </div>    
         </div>
-      </div>   
-      <div className="seconddivx">
-        <div className="Recentfeed">
-          <div className="recentmessagediv">
-              <div className="recentmessagediv">
-                  <MailIcon />
-              </div>
-              <div className="recentmessagediv">
-                  <h5>Recent General Feeds</h5>
-              </div>
+      </div>
+      <div className="shifabg">
+        <div className="seconddivx">
+          <div className="Recentfeed">
+            <div className="recentmessagediv">
+                <div className="recentmessagediv">
+                    <MailIcon />
+                </div>
+                <div className="recentmessagediv">
+                    <h5>Recent General Feeds</h5>
+                </div>
+            </div>
+          </div>
+          <div className="searchrecentfeed">
+            <div className='navbarSearch'>
+              <input className="formble" type="text" placeholder="Search" aria-label="Search" />             
+            </div>
           </div>
         </div>
-        <div className="searchrecentfeed">
-          <div className='navbarSearch'>
-            <input className="formble" type="text" placeholder="Search" aria-label="Search" />
-            
-          </div>
+        <div className="biggerdivmove">
+        <div className="bigerrdiva">
+            <div className="avatarhead">
+              <div className="avatardiv">
+                <div className="profilepic">
+                  <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />
+                </div>
+                <div className="Time">
+                  <h4>Peter Olayinka</h4>
+                </div>
+              </div>
+              <div className="avatartime">
+                <p>Tue 24 May,2022 04:01:98</p>
+              </div>
+            </div>
+          <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+          <br />
+          <hr />
         </div>
-      </div>
-      <div className="bigerrdiva">
-          <div className="avatarhead">
-            <div className="avatardiv">
-              <div className="profilepic">
-                 <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />
+        <div className="bigerrdiva">
+            <div className="avatarhead">
+              <div className="avatardiv">
+                <div className="profilepic">
+                  <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />
+                </div>
+                <div className="Time">
+                  <h4>Peter Olayinka</h4>
+                </div>
               </div>
-              <div className="Time">
-                <h4>Peter Olayinka</h4>
-              </div>
-            </div>
-            <div className="avatartime">
-              <p>Tue 24 May,2022 04:01:98</p>
-            </div>
-          </div>
-        <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-        <br />
-        <hr />
-      </div>
-      <div className="bigerrdiva">
-          <div className="avatarhead">
-            <div className="avatardiv">
-              <div className="profilepic">
-                 <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />
-              </div>
-              <div className="Time">
-                <h4>Peter Olayinka</h4>
+              <div className="avatartime">
+                <p>Tue 24 May,2022 04:01:98</p>
               </div>
             </div>
-            <div className="avatartime">
-              <p>Tue 24 May,2022 04:01:98</p>
-            </div>
-          </div>
-        <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-        <br />
-        <hr />
-      </div>
-      <div className="bigerrdiva">
-          <div className="avatarhead">
-            <div className="avatardiv">
-              <div className="profilepic">
-                 <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />
+          <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+          <br />
+          <hr />
+        </div>
+        <div className="bigerrdiva">
+            <div className="avatarhead">
+              <div className="avatardiv">
+                <div className="profilepic">
+                  <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />
+                </div>
+                <div className="Time">
+                  <h4>Peter Olayinka</h4>
+                </div>
               </div>
-              <div className="Time">
-                <h4>Peter Olayinka</h4>
-              </div>
-            </div>
-            <div className="avatartime">
-              <p>Tue 24 May,2022 04:01:98</p>
-            </div>
-          </div>
-        <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-        <br />
-        <hr />
-      </div>
-      <div className="bigerrdiva">
-          <div className="avatarhead">
-            <div className="avatardiv">
-              <div className="profilepic">
- <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />                
-              </div>
-              <div className="Time">
-                <h4>Peter Olayinka</h4>
+              <div className="avatartime">
+                <p>Tue 24 May,2022 04:01:98</p>
               </div>
             </div>
-            <div className="avatartime">
-              <p>Tue 24 May,2022 04:01:98</p>
+          <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+          <br />
+          <hr />
+        </div>
+        <div className="bigerrdiva">
+            <div className="avatarhead">
+              <div className="avatardiv">
+                <div className="profilepic">
+  <img className='profilepix' width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBbr_GF0rPVY0W_eatFSF92HkphRwtCLfHag&usqp=CAU" />                
+                </div>
+                <div className="Time">
+                  <h4>Peter Olayinka</h4>
+                </div>
+              </div>
+              <div className="avatartime">
+                <p>Tue 24 May,2022 04:01:98</p>
+              </div>
             </div>
-          </div>
-        <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-        <br />
-        <hr />
-      </div>
+          <p>dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+          <br />
+          <hr />
+        </div>
+        </div>
+          
+        </div>
     </section>
   )
 }
